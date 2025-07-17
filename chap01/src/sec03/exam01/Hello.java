9b1003507a38c79cc301bb2dd35dc497d3b320d7
JM Bae <bjm91962051@gmail.com>
2025-07-17 14:21:05 +0900

    feat: add practice code for chap01

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,21 @@
+package sec03.exam01;
+
+
+/*
+ 작성자: assignmin
+ 작성일: 2025.05.09
+ */
+
+// 위에는 범위주석
+// 아래는 라인주석
+
+//프로그램 실행 진입정
+public class Hello {
+
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+
+	} // end of main
+
+}  // end of class
